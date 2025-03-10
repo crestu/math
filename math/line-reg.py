0a2f70c45f3966b272ff80164cc1001ceebe9416
Creed <creedstudenski@gmail.com>
2025-03-09 19:45:04 -0500

    renaming, fix output, formatting

--- math/line-reg.py
+++ math/line-reg.py
@@ -6,29 +6,29 @@
 #define y
 #calculate Ex
 #calculate Ey
+#function to calculate Ex**2
+#function to calculate Exy
+# create linear regression function
 
-x=[3,9,5,3]
-y=[8,6,4,2]
+x = [3, 9, 5, 3]
+y = [8, 6, 4, 2]
 n = len(x)
 
-Ex = sum(x)  
-Ey= sum(y)
-
-#function to calculate Ex**2
-def Exq(x):
+def Ex_squared(x):
     return sum(i**2 for i in x)
 
-#function to calculate Exy
-def Exy(x,y):
-    return sum(i*j for i,j in zip(x,y))
+def Exy(x, y):
+    return sum(i * j for i, j in zip(x, y))
             
-# create linear regression function
-def line_reg(x,y):
-    a = ((Ey)*(Exq(x)) - (Ex)*(Exy(x, y))) / ((n * Exq(x)) - (Ex)**2)
-    b = ((n * Exy(x, y)) - (Ex * Ey)) / ((n * Exq(x)) - (Ex)**2)
+def linear_regression(x, y):
+    Ex = sum(x)
+    Ey = sum(y)
+    
+    a = ((Ey * Ex_squared(x)) - (Ex * Exy(x, y))) / ((n * Ex_squared(x)) - Ex**2)
+    b = ((n * Exy(x, y)) - (Ex * Ey)) / ((n * Ex_squared(x)) - Ex**2)
 
-    ans = a+b
-    return ans
-result = line_reg(x, y)
+    return a, b
 
-print(result)
+a, b = linear_regression(x, y)
+
+print(f"y = {a:.2f} + {b:.2f}x")

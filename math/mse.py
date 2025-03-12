cc700c928ea53f69d49824ec82107f1e29cff349
Creed <creedstudenski@gmail.com>
2025-03-12 12:24:41 -0500

    corrected, commited wrong version

--- math/mse.py
+++ math/mse.py
@@ -21,29 +21,9 @@ x = [1, 2, 3, 4]
 y = [2, 3, 5, 7]
 n = len(x)
 
-def Ex_squared(x):
-    return sum(i**2 for i in x)
-
-def Exy(x, y):
-    return sum(i * j for i, j in zip(x, y))
-            
-def linear_regression(x, y):
-    Ex = sum(x)
-    Ey = sum(y)
-    
-    a = ((Ey * Ex_squared(x)) - (Ex * Exy(x, y))) / ((n * Ex_squared(x)) - Ex**2)
-    b = ((n * Exy(x, y)) - (Ex * Ey)) / ((n * Ex_squared(x)) - Ex**2)
-
-    return a, b
-
-
 def mean_square_error(x,y):
     return sum((i-j)**2 for i, j in zip(x,y))/n
-
 
 ans= mean_square_error(x,y)
 
 print(f"Mean Square Error = {ans}")
-
-
-

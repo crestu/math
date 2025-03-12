cb7bce5139323f17daef68cc4d4640703635292e
Creed <creedstudenski@gmail.com>
2025-03-12 12:23:33 -0500

    final rewrite of MSE formula

--- math/mse.py
+++ math/mse.py
@@ -14,21 +14,34 @@
 #divide sum_of by n
 
 #edge cases
+#empty lists
+#unequal lists
+
+x = [1, 2, 3, 4]
+y = [2, 3, 5, 7]
+n = len(x)
+
+def Ex_squared(x):
+    return sum(i**2 for i in x)
+
+def Exy(x, y):
+    return sum(i * j for i, j in zip(x, y))
+            
+def linear_regression(x, y):
+    Ex = sum(x)
+    Ey = sum(y)
+    
+    a = ((Ey * Ex_squared(x)) - (Ex * Exy(x, y))) / ((n * Ex_squared(x)) - Ex**2)
+    b = ((n * Exy(x, y)) - (Ex * Ey)) / ((n * Ex_squared(x)) - Ex**2)
+
+    return a, b
+
 
 def mean_square_error(x,y):
+    return sum((i-j)**2 for i, j in zip(x,y))/n
 
-    n = len(x)
-    yi  = None
-    yj= None
 
-    error = yi-yj
-    me_sum = sum(error)
-
-    ans = me_sum/n
-
-    return ans
-
-ans= mean_square_error()
+ans= mean_square_error(x,y)
 
 print(f"Mean Square Error = {ans}")
 
